a7d79b52cd7ff89e170f5a3c5c8bb220d5401154
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-06-23 20:30:20 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 
-// const root = ReactDom.createRoot(document.getElementById('main'));
-// root.render(<App/>);
-
 ReactDom.render(
   <App/>,
   document.getElementById('main')
